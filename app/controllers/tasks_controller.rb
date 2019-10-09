2bb7ee7df1b61fe2e8c4f5597339ced838be4d6e
hiroyuki.inuyama <kaigaigame34@gmail.com>
2019-10-09 09:54:10 +0000

    syuusei

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy,:update,:show]
   def index
      if logged_in?
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
@@ -23,7 +24,7 @@ class TasksController < ApplicationController
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to root_url
     else
-    
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
